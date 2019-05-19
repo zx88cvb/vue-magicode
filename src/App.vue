b3bdb76288479716afb3f27c0f322833e1534831
zx88cvb <295389754@qq.com>
2019-05-19 22:16:38 +0800

    接入友盟统计工具

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,37 @@ export default {
     MHeader,
     MFooter,
     TopScrool
+  },
+  mounted () {
+    this.cnzz()
+  },
+  methods: {
+    cnzz () {
+      // 创建cnzz统计js
+      const script = document.createElement('script')
+      script.src = 'https://s96.cnzz.com/z_stat.php?id=1277622426&web_id=1277622426'
+      script.language = 'JavaScript'
+      document.body.appendChild(script)
+    }
+  },
+  watch: {
+    '$route': {
+      handler (to, from) {
+        // 避免首次获取不到window._czc
+        setTimeout(() => {
+          if (window._czc) {
+            let location = window.location
+            let contentUrl = location.pathname + location.hash
+            let refererUrl = '/'
+            // 用于发送某个URL的PV统计请求，
+            window._czc.push(['_trackPageview', contentUrl, refererUrl])
+            window._czc.push(['_setAutoPageview', false])
+          }
+        }, 300)
+      },
+      // 首次进入页面即执行
+      immediate: true
+    }
   }
 }
 </script>
