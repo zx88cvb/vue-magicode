ee7e01b3581bd6818204d6def0fd87e7a3e42330
zx88cvb <295389754@qq.com>
2020-03-08 16:30:23 +0800

    icp

--- src/components/footer/MFooter.vue
+++ src/components/footer/MFooter.vue
@@ -9,6 +9,7 @@
           <p>Copyright © 2019 Angelive
             · Powered By Vue · Theme By Angel
           </p>
+          <a href="http://www.beian.miit.gov.cn" target="view_window">鄂ICP备19006922号-1</a>
         </div>
       </div>
     </div>
@@ -32,7 +33,7 @@ export default {
           color: #e0e0e0
           margin-right: 10px
       .copyright-footer
-        p
+        p, a
           color: #828282
           font-size: 12px
 </style>
